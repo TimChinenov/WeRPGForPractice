46bc330a8ac529a8c6890f1256a26b17c0aa52ea
Timmy <tim.chinenov@gmail.com>
2017-07-01 12:56:25 -0400

    added main file to project as well as two simple functions.Lets see how long this idea lasts

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+// include libraries
+#include <iostream>
+#include <string>
+
+//include headers below here
+
+//prototyp functions below
+char save_or_load();
+void starting_game(char choice);
+// ============================================================================
+// ============================================================================
+
+//Start main here
+int main (int argc, char* argv[]) {
+  char choice; //This variable holds results of deccistions made
+  choice = save_or_load();//get user choice
+  starting_game(choice);
+}
+
+
+//the following function will initiate the basic introduction to the game
+char save_or_load(){
+  char action;
+  std::cout << "Welcome to WeRPGForPractice\n";
+  std::cout << "Would you likst to start a new game or load a game [N/L]\n";
+  std::cin >> action;
+  return action;
+
+}
+
+void starting_game(char choice)
+{
+  if (choice == 'n' || choice == 'N' || choice == 'new' || choice == 'New')
+  {
+      //start new game here if the choice was new
+    std::cout << "Starting new game...";
+  }
+  else if (choice == 'l' || choice || 'L' || choice == 'load' ||\
+   choice == 'load')
+  {
+    //load list of all games
+    std::cout << "Load games...";
+  }
+  else //if statement is not interpreted
+  {
+    std::cout << "LOL can you not follow basic instructions?";
+  }
+}
